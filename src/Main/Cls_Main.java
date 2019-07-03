297fa6a1958414b8d0c474031e5c6fc93b73e141
cllanos <cllanos@PCCRISTHIAN.local.iest.com>
2019-07-03 13:30:15 +0200

    Inicial MotorV1

---
+++ src/Main/Cls_Main.java
@@ -0,0 +1,15 @@
+//Modificado 23:06:54
+package Alarma;
+
+import Formularios.Frm_Motor;
+
+
+public class Cls_Main {
+
+    public static void main(String[] args) {
+        Frm_Motor FM = new Frm_Motor();
+        FM.setVisible(true);
+
+        
+    }
+}
