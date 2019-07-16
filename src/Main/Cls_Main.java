3e2bccb5068a214933e8c2a4b6732df79322af7d
cllanos <cllanos@PCCRISTHIAN.local.iest.com>
2019-07-16 11:38:13 +0200

    Testing 2

--- src/Main/Cls_Main.java
+++ src/Main/Cls_Main.java
@@ -1,14 +1,14 @@
 //Modificado 23:06:54
 package Alarma;
 
-import Formularios.Frm_Motor;
+import Formularios.Frm_Main;
 
 
 
 public class Cls_Main {
 
     public void main(String[] args) {
-        Frm_Motor FM = new Frm_Motor();
+        Frm_Main FM = new Frm_Main();
         
         FM.setVisible(true);
 
